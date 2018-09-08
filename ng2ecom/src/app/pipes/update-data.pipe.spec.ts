2cd5f0ad284785e6be482b7508d2cbb800263de1
ffacon <francois.facon@gmail.com>
2018-09-08 15:56:15 +0200

    add pipes

---
+++ ng2ecom/src/app/pipes/update-data.pipe.spec.ts
@@ -0,0 +1,31 @@
+import {ReflectiveInjector, Injector} from '@angular/core';
+import { TestBed, async } from '@angular/core/testing';
+import { UpdateDataPipe } from './update-data.pipe';
+
+import {DataContainerService} from '../services/data-container.service';
+
+let dataContainerMock: any;
+
+ beforeEach(() => {
+
+ dataContainerMock = { filteredBooks: '' };
+    TestBed.configureTestingModule({
+       providers: [
+         {provide: DataContainerService, useClass: dataContainerMock}
+      ]
+    }).compileComponents();
+ });
+
+describe('UpdateDataPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UpdateDataPipe(dataContainerMock);
+    expect(pipe).toBeTruthy();
+  });
+
+it('Update data works', function() {
+ const pipe = new UpdateDataPipe(dataContainerMock);
+ const newContent = 'new field content';
+ pipe.transform(newContent, []);
+ expect(dataContainerMock.filteredBooks).toEqual(newContent);
+ });
+});
---
+++ ng2ecom/src/app/pipes/update-data.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform, Inject } from '@angular/core';
+import { DataContainerService } from '../services/data-container.service';
+
+@Pipe({
+  name: 'updateData'
+})
+export class UpdateDataPipe implements PipeTransform {
+
+  constructor( @Inject(DataContainerService) public dataContainer: DataContainerService) {}
+
+  transform(input: any, args: any) {
+    this.dataContainer.filteredBooks = input;
+    return input;
+  }
+
+
+}
